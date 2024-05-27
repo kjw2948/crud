870896193ef4c3436d9b08dec267f8e5380fb973
kjw2948 <kjw970313@cau.ac.kr>
2024-05-27 21:22:36 +0900

    [update] Entity를 DTO 객체로 변환하는 로직 추가

--- src/main/java/crud/spring/dto/BoardDTO.java
+++ src/main/java/crud/spring/dto/BoardDTO.java
@@ -3,9 +3,11 @@ package crud.spring.dto;
 
 //DTO --> 어떤 데이터의 한세트를 묶어서 DTO 클래스를 만들어서 데이터를 전송할 때 사용한다.
 
+import crud.spring.entity.BoardEntity;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Getter @Setter
 @ToString
@@ -23,4 +25,18 @@ public class BoardDTO {
     private int boardHits; // 조회수
     private LocalDateTime boardCreatedTime; //글 작성시간
     private LocalDateTime boardUpdateTime; // 글 수정시간
+
+    public static BoardDTO toBoardDTO(BoardEntity boardEntity) {
+        BoardDTO boardDTO = new BoardDTO();
+        boardDTO.setId(boardEntity.getId());
+        boardDTO.setBoardWriter(boardEntity.getBoardWriter());
+        boardDTO.setBoardTitle(boardEntity.getBoardTitle());
+        boardDTO.setBoardPass(boardEntity.getBoardPass());
+        boardDTO.setBoardContents(boardEntity.getBoardContents());
+        boardDTO.setBoardHits(boardEntity.getBoardHits());
+        boardDTO.setBoardCreatedTime(boardEntity.getCreatedTime());
+        boardDTO.setBoardUpdateTime(boardEntity.getUpdatedTime());
+
+        return boardDTO;
+    }
 }
