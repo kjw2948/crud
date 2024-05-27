8b9bea438f2bf9f89b2af6b3b5e7d15098d63e23
kjw2948 <kjw970313@cau.ac.kr>
2024-05-27 21:23:03 +0900

    [update] 조회수 늘어나는 메소드 구현

--- src/main/java/crud/spring/repository/BoardRepository.java
+++ src/main/java/crud/spring/repository/BoardRepository.java
@@ -2,6 +2,9 @@ package crud.spring.repository;
 
 import crud.spring.entity.BoardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
@@ -9,4 +12,9 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
+    //update board_table set board_hits=board_hits + 1 where id = ?
+    @Modifying  // update, delete 등의 쿼리를 실행할 때는 해당쿼리를 필수로 붙이기!
+    @Query(value = "update BoardEntity b set b.boardHits = b.boardHits+1 where b.id = :id")
+    //JPA에서 제공하는 어노테이션
+    void updateHits(@Param("id") Long id);
 }
