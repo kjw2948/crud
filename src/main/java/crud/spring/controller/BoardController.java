568ca92e8c6b4fbb4aa54a2865d6250f77e7342c
kjw2948 <kjw970313@cau.ac.kr>
2024-05-28 21:13:58 +0900

    [update]

--- src/main/java/crud/spring/controller/BoardController.java
+++ src/main/java/crud/spring/controller/BoardController.java
@@ -4,10 +4,10 @@ import crud.spring.dto.BoardDTO;
 import crud.spring.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/board")
@@ -23,8 +23,89 @@ public class BoardController {
 
     @PostMapping("/save")
     public String save(@ModelAttribute BoardDTO boardDTO) {
+        boardService.save(boardDTO);
+        return "index";
+    }
+
+    @GetMapping("/")
+    public String findAll(Model model) {
+        List<BoardDTO> boardDTOList = boardService.findAll();
+        model.addAttribute("boardList", boardDTOList);
+        return "list";
+    }
+
+    @GetMapping("/{id}")
+    public String findById(@PathVariable Long id, Model model) {
+        boardService.updateHits(id);
+        BoardDTO board = boardService.findById(id);
+        model.addAttribute("board", board);
+        return "detail";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
+    @GetMapping("/save")
+    public String saveForm() {
+        return "save";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute BoardDTO boardDTO) {
         System.out.println("boardDTO = " + boardDTO);
         boardService.save(boardDTO);
         return "index";
     }
+
+    @GetMapping("/")
+    public String findAll(Model model) {
+        List<BoardDTO> boardDTOList = boardService.findAll();
+        model.addAttribute("boardList", boardDTOList);
+        return "list";
+    }
+
+    @GetMapping("/{id}")
+    public String findById(@PathVariable Long id, Model model) {
+        /*
+        해당 게시글의 조회수를 하나 올리고 게시글 데이터를 가져와서 detail.html에 출력
+         */
+    /*
+        boardService.updateHits(id);
+        BoardDTO boardDTO = boardService.findById(id);
+        model.addAttribute("board", boardDTO);
+        return "detail";
+    }
+    */
 }
--- src/main/java/crud/spring/controller/HomeController.java
+++ src/main/java/crud/spring/controller/HomeController.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+
     @GetMapping("/")
     public String index() {
         return "index";
     }
+
 
 }
